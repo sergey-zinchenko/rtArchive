eb36f249431d08f269317f1206ff4f035f5d9ecf
alex <norther48@list.ru>
2018-10-29 15:33:19 +0400

    return id instead RT in save func return void instead RT in addResponse func

--- storage/storage.go
+++ storage/storage.go
@@ -4,17 +4,20 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/kataras/iris/core/errors"
+	// empty inport pq package
 	_ "github.com/lib/pq"
 	"rtArchive/config"
 	"rtArchive/storage/queries"
 )
 
+//DBS struct provide connection to db
 type (
 	DBS struct {
 		pgSQL *sql.DB
 	}
 )
 
+//Connect - connect to db
 func (dbs *DBS) Connect() (err error) {
 	if dbs.pgSQL, err = sql.Open("postgres", config.PgSqlDSN); err != nil {
 		return err
@@ -25,6 +28,7 @@ func (dbs *DBS) Connect() (err error) {
 	return nil
 }
 
+//CheckTables - transaction for db instantiation/check
 func (dbs *DBS) CheckTables() (err error) {
 	if dbs.pgSQL == nil {
 		return errors.New("pgSql database not connected")
